25f08704bcefd7ff7119053b97196b67aa077963
Kenneth B <kennethzhell@gmail.com>
2023-03-31 21:19:19 +0200

    Feature Request: System Agnostic #4

--- setup.mjs
+++ setup.mjs
@@ -3,18 +3,20 @@ import {
   findContained,
   findContainers
 } from "./scripts/api.mjs";
-import { MODULE } from "./scripts/constants.mjs";
+import {MODULE} from "./scripts/constants.mjs";
 import {
   _createHeaderButton,
   _createTemplate,
   _deleteTemplate,
   _preCreateTemplate,
+  _preUpdateCombat,
   _preUpdateTemplate,
   _preUpdateToken,
+  _updateCombat,
   _updateTemplate,
   _updateToken
 } from "./scripts/hooks.mjs";
-import { callMacro } from "./scripts/templatemacro.mjs";
+import {callMacro} from "./scripts/templatemacro.mjs";
 
 Hooks.once("setup", () => {
   game.modules.get(MODULE).api = {
@@ -26,14 +28,17 @@ Hooks.once("setup", () => {
   MeasuredTemplateDocument.prototype.callMacro = async function(type = "never", options = {}) {
     return callMacro(this, type, options);
   }
-})
-
-Hooks.on("getMeasuredTemplateConfigHeaderButtons", _createHeaderButton);
-Hooks.on("getItemSheetHeaderButtons", _createHeaderButton);
-Hooks.on("preUpdateToken", _preUpdateToken);
-Hooks.on("updateToken", _updateToken);
-Hooks.on("preCreateMeasuredTemplate", _preCreateTemplate);
-Hooks.on("createMeasuredTemplate", _createTemplate);
-Hooks.on("deleteMeasuredTemplate", _deleteTemplate);
-Hooks.on("preUpdateMeasuredTemplate", _preUpdateTemplate);
-Hooks.on("updateMeasuredTemplate", _updateTemplate);
+  if (game.system.id === "dnd5e") {
+    Hooks.on("getItemSheetHeaderButtons", _createHeaderButton);
+    Hooks.on("preCreateMeasuredTemplate", _preCreateTemplate);
+  }
+  Hooks.on("createMeasuredTemplate", _createTemplate);
+  Hooks.on("deleteMeasuredTemplate", _deleteTemplate);
+  Hooks.on("getMeasuredTemplateConfigHeaderButtons", _createHeaderButton);
+  Hooks.on("preUpdateMeasuredTemplate", _preUpdateTemplate);
+  Hooks.on("preUpdateToken", _preUpdateToken);
+  Hooks.on("updateMeasuredTemplate", _updateTemplate);
+  Hooks.on("updateToken", _updateToken);
+  Hooks.on("preUpdateCombat", _preUpdateCombat);
+  Hooks.on("updateCombat", _updateCombat);
+});
